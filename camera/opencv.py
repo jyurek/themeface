d0d6154ae12f87423f8a5b658dad5536769bfd79
Jon Yurek <jyurek@thoughtbot.com>
2017-06-16 11:39:29 -0400

    Allow for rotation

--- camera/opencv.py
+++ camera/opencv.py
@@ -13,5 +13,18 @@ class Camera(object):
         # return cv2.resize(frame, self.resolution)
         return cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
+    def rotate(self, image, rotation=0):
+        if rotation == 90:
+            out = cv2.transpose(image)
+            return cv2.flip(out, flipCode=1)
+        elif rotation == 180:
+            out = cv2.flip(image, flipCode=0)
+            return cv2.flip(out, flipCode=1)
+        elif rotation == 270:
+            out = cv2.transpose(image)
+            return cv2.flip(out, flipCode=0)
+        else:
+            return image
+
     def release(self):
         self.camera.release()
--- camera/pi.py
+++ camera/pi.py
@@ -14,5 +14,18 @@ class Camera(object):
         self.camera.capture(self.output_buffer, format="bgr")
         return self.output_buffer
 
+    def rotate(self, image, rotation=0):
+        if rotation == 90:
+            out = np.transpose(image)
+            return np.fliplr(out)
+        elif rotation == 180:
+            out = np.flipud(image)
+            return np.fliplr(out)
+        elif rotation == 270:
+            out = np.transpose(image)
+            return np.flipud(out)
+        else:
+            return image
+
     def release(self):
         "Ok, cool."
